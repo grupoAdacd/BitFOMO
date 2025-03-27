f804075f9e28f13782e23bf849ea89e3fedd4b67
d4rk <herreragp334@gmail.com>
2025-03-27 12:46:26 +0000

    fetchTodayFluctuation method developed

--- src/main/java/APIs/BinanceAPI.java
+++ src/main/java/APIs/BinanceAPI.java
@@ -1,4 +1,29 @@
 package APIs;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.net.http.HttpRequest;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class BinanceAPI {
+    private static final String BASE_URL = "https://api.binance.com";
+
+    public JSONObject fetchTodayFluctuation(String correlation) throws IOException, InterruptedException {
+        String apiURL = BASE_URL + "/api/v3/ticker/24hr?symbol=" + correlation;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(apiURL))
+                .build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String bodyResponse = response.body();
+
+        return new JSONObject(bodyResponse);
+
+    }
+
 }
